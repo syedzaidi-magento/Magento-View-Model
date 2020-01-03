3ba4d14c8747e87ae192895c5663aad6604a7a7d
syedzaidi-magento <syedqurat27@gmail.com>
2020-01-02 22:27:12 -0800

    add files

---
+++ Controller/Index/Index.php
@@ -0,0 +1,46 @@
+<?php
+namespace Syedzaidi\Mvvm\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+/**
+ * Class Index
+ * @package Syedzaidi\Mvvm\Controller\Index
+ */
+class Index extends Action
+{
+    /**
+     * @var Context
+     */
+    private $context;
+    /**
+     * @var PageFactory
+     */
+    private $pageFactory;
+
+    /**
+     * Index constructor.
+     * @param Context $context
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $pageFactory
+    ){
+        parent::__construct($context);
+
+        $this->context = $context;
+        $this->pageFactory = $pageFactory;
+    }
+
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface|\Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        return $this->pageFactory->create();
+
+    }
+}
---
+++ ViewModel/MyNewViewModel.php
@@ -0,0 +1,12 @@
+<?php
+namespace Syedzaidi\Mvvm\ViewModel;
+
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+
+class MyNewViewModel implements ArgumentInterface
+{
+    public function getPageTitle()
+    {
+        return "M.V.V.M Module Output.";
+    }
+}
